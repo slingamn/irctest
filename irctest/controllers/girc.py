bc8a650effd5ff3575d0df6a6245a738e46da061
Daniel Oaks <daniel@danieloaks.net>
2015-12-20 10:16:46 +1000

    irctest.controllers.girc: Update for SASL

--- irctest/controllers/girc.py
+++ irctest/controllers/girc.py
@@ -7,17 +7,32 @@ class GircController(BaseClientController):
         super().__init__()
         self.directory = None
         self.proc = None
+
+    def kill(self):
+        if self.proc:
+            self.proc.terminate()
+            try:
+                self.proc.wait(5)
+            except subprocess.TimeoutExpired:
+                self.proc.kill()
+            self.proc = None
+
     def __del__(self):
         if self.proc:
             self.proc.kill()
         if self.directory:
             self.directory.cleanup()
 
-    def run(self, hostname, port, authentication):
+    def run(self, hostname, port, auth):
+        args = ['--host', hostname, '--port', str(port), '--quiet']
+
+        if auth:
+            args += ['--sasl-name', auth.username]
+            args += ['--sasl-pass', auth.password]
+            args += ['--sasl-fail-is-ok']
+
         # Runs a client with the config given as arguments
-        self.proc = subprocess.Popen(['girc_test', 'connect',
-            '--host', hostname, '--port', str(port),
-            '--quiet'])
+        self.proc = subprocess.Popen(['girc_test', 'connect'] + args)
 
 def get_irctest_controller_class():
     return GircController
